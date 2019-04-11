b267198d5b421f6782073c2c43d0d27d0b30b7aa
ChoeHB <luiz4our@naver.com>
2019-04-12 02:35:05 +0900

    에셋번들 관련코드

---
+++ ChoeHB/AssetBundle/Editor/BuildAssetBundles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetBundles : MonoBehaviour {
+
+    private const BuildAssetBundleOptions option    = BuildAssetBundleOptions.None;
+    private const BuildTarget buildTarget           = BuildTarget.Android;
+
+    [MenuItem("Bundles/Build AssetBundles")]
+    public static void BuildAllAssetBundles()
+        => BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", option, buildTarget);
+
+}
---
+++ ChoeHB/AssetBundle/LoadAssetBundle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadAssetBundle : MonoBehaviour {
+
+    private static LoadAssetBundle instance;
+
+    public static void LoadLocal(string bundleName, int version, Action<AssetBundle> callback)
+        => Load(Application.streamingAssetsPath + "/" + bundleName, version, callback);
+
+    public static void Load(string bundleURL, int version, Action<AssetBundle> callback)
+    {
+        if (instance == null)
+            instance = new GameObject("LoadAssetBundle").AddComponent<LoadAssetBundle>();
+        instance.StartCoroutine(instance.DownloadAndCache(bundleURL, version, callback));
+    }
+
+    private IEnumerator DownloadAndCache(string bundleURL, int version, Action<AssetBundle> callback)
+    {
+        while(!Caching.ready)
+            yield return null;
+        using (UnityWebRequest uwr = UnityWebRequestAssetBundle.GetAssetBundle(bundleURL))
+        {
+            yield return uwr.SendWebRequest();
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
+            callback(bundle);
+        }
+    }
+}
